5023702dc51b1a65d6357744ce4633053675f628
DenProstoDen <vuda.vvv@mail.ru>
2024-02-02 16:05:10 +0400

    fix

--- frontend/src/utils/Sign.js
+++ frontend/src/utils/Sign.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "export const BASE_URL = 'api.mesto.cohort77.nomoredomainsmonster.ru";
 
 const getResponse = (res) => {
   return res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`);
