622c7c6c6e111789459e5cf90423278b3c66148d
DenProstoDen <vuda.vvv@mail.ru>
2024-02-04 14:25:39 +0400

    fix add back

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -1,53 +1,22 @@
 const router = require('express').Router();
-// eslint-disable-next-line import/no-extraneous-dependencies
-const { celebrate, Joi } = require('celebrate');
-const urlReg = require('../utils/constants');
 
 const {
   createCard, getCards, deleteCard, likeCard, dislikeCard,
 } = require('../controllers/cards');
 
-router.get('/cards', getCards); // возвращает все карточки
+const {
+  postCreatCardValidate,
+  fetchCardValidate,
+} = require('../utils/validation');
 
-router.post(
-  '/cards',
-  celebrate({
-    body: Joi.object().keys({
-      name: Joi.string().required().min(2).max(30),
-      link: Joi.string().required().pattern(urlReg),
-    }),
-  }),
-  createCard,
-);
+router.get('/', getCards);
 
-router.delete(
-  '/cards/:cardId',
-  celebrate({
-    params: Joi.object().keys({
-      cardId: Joi.string().alphanum().length(24).required(),
-    }),
-  }),
-  deleteCard,
-);
+router.post('/', postCreatCardValidate, createCard);
 
-router.put(
-  '/cards/:cardId/likes',
-  celebrate({
-    params: Joi.object().keys({
-      cardId: Joi.string().alphanum().length(24).required(),
-    }),
-  }),
-  likeCard,
-);
+router.delete('/:cardId', fetchCardValidate, deleteCard);
 
-router.delete(
-  '/cards/:cardId/likes',
-  celebrate({
-    params: Joi.object().keys({
-      cardId: Joi.string().alphanum().length(24).required(),
-    }),
-  }),
-  dislikeCard,
-);
+router.put('/:cardId/likes', fetchCardValidate, likeCard);
+
+router.delete('/:cardId/likes', fetchCardValidate, dislikeCard);
 
 module.exports = router;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,41 +1,19 @@
 const router = require('express').Router();
-// eslint-disable-next-line import/no-extraneous-dependencies
-const { celebrate, Joi } = require('celebrate');
 const { createUser, login } = require('../controllers/users');
 const auth = require('../middlewares/auth');
 const NotFoundError = require('../errors/NotFoundError');
 const userRout = require('./users');
 const cardRout = require('./cards');
-const urlReg = require('../utils/constants');
+const { signInValid, signUpValid } = require('../utils/validation');
 
-router.post(
-  '/signin',
-  celebrate({
-    body: Joi.object().keys({
-      email: Joi.string().required().email(),
-      password: Joi.string().required(),
-    }),
-  }),
-  login,
-);
+router.post('/signin', signInValid, login);
 
-router.post(
-  '/signup',
-  celebrate({
-    body: Joi.object().keys({
-      name: Joi.string().min(2).max(30),
-      about: Joi.string().min(2).max(30),
-      avatar: Joi.string().pattern(urlReg),
-      email: Joi.string().required().email(),
-      password: Joi.string().required(),
-    }),
-  }),
-  createUser,
-);
+router.post('/signup', signUpValid, createUser);
 
 router.use(auth);
-router.use('/', userRout);
-router.use('/', cardRout);
+
+router.use('/users', userRout);
+router.use('/cards', cardRout);
 
 router.use((req, res, next) => {
   next(new NotFoundError('Страница не найдена'));
