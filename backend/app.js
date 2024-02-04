0b8eadff6a41834fa91e113912a7c0fd1cf00abc
DenProstoDen <vuda.vvv@mail.ru>
2024-02-04 14:38:53 +0400

    fix

--- backend/app.js
+++ backend/app.js
@@ -2,24 +2,34 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 
-const { PORT = 3000 } = process.env;
+const { errors } = require('celebrate');
+const { PORT, MONGO_URL } = require('./utils/config');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 // eslint-disable-next-line import/no-extraneous-dependencies
-const { errors } = require('celebrate');
 
 const app = express();
 
 const router = require('./routes/index');
 const { handleCenterError } = require('./middlewares/handleCenterError');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect(MONGO_URL);
+
+app.use(requestLogger);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 
 app.use(router);
+
+app.use(errorLogger);
 
 app.use(errors());
 
