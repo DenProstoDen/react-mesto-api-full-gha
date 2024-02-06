57570103b29d3ac87f90cbbc21d73f20c86c79c9
DenProstoDen <vuda.vvv@mail.ru>
2024-02-06 22:35:36 +0400

    fix

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -1,83 +1,97 @@
 class Api {
-    constructor({url}) {
-      this._url = url
+  constructor (options) {
+    this._url = options.baseUrl;
   }
 
-    _request(endpoint, options = {}) {
-      const token = localStorage.getItem('jwt')
+   _request(endpoint, options = {}) {
+    const token = localStorage.getItem('jwt')
+    return fetch(
+    `${ this._url }/${endpoint}`,
+    {headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`
+            }, ...options})
+    .then(this._checkResponse)
+  }
 
-      return fetch(
-          `${ this._url }/${endpoint}`,
-          {headers: {
-                      'Content-Type': 'application/json',
-                      Authorization: `Bearer ${token}`
-                  }, ...options})
-          .then(this._checkResponse)
-    }
-  
-    _checkResponse(res) {
-    return res.ok ? res.json() : Promise.reject(`Ошибка ${res.status}`); 
-    }
-  
-    getName() {
-      return this._request('users/me', {
-        method: 'GET'
-    })
+  _checkResponse(res) {
+  return res.ok ? res.json() : Promise.reject(`Ошибка ${res.status}`); 
   }
-  
-    getCards() {
-      return  this._request('cards', {
-        method: 'GET'
+
+  getName() {
+    return this._request('users/me', {
+      method: 'GET'
+  })
+}
+
+  getCards() {
+    return this._request('cards', {
+      method: 'GET'
+  })
+}
+
+  editProfileInfo(data) {
+    return this._request('users/me',  {
+      method: 'PATCH',
+      body: JSON.stringify({
+          name: data.name,
+          about: data.info
       })
-  }
-  
-    editProfileInfo(data) {
-      return this._request('users/me',  {
-        method: 'PATCH',
-        body: JSON.stringify({
-            name: data.name,
-            about: data.about
-        })
-    })
-  }
-  
-    setNewAvatar(data) {
-      return this._request('users/me/avatar', {
-        method: 'PATCH',
-        body: JSON.stringify({
-            avatar: data.avatar
+  })
+}
+
+  setNewAvatar(data) {
+    return this._request('users/me/avatar', {
+      method: 'PATCH',
+      body: JSON.stringify({
+          avatar: data.avatar
       })
-    })
-  }
-  
-    addCard(data) {
-      return this._request('cards', {
-        method: 'POST',
-        body: JSON.stringify(data)
-      })
-    }
-  
-    addLike(cardId, like) {
-      return this._request(`cards/${cardId}/likes`, {
-        method: like ? 'PUT' : 'DELETE'
-    })
-  }
-  
-    deleteCard(cardId) {
-      return this._request(`cards/${cardId}`, {
-        method: 'DELETE'
     })
   }
 
-    changeLikeCardStatus(cardId, like) {
-      return this._request(`cards/${cardId}/likes`, {
-        method: like ? 'PUT' : 'DELETE'
-      })
-    }
+  addCard(data) {
+    return this._request('cards', {
+      method: 'POST',
+      body: JSON.stringify({ 
+        name: data.placename,
+        link: data.link })
+    })
   }
 
-  const api = new Api({
-    url: 'https://api.denmyname.nomoredomainswork.ru',
-  });
-  
-  export default api;
+  // addLike(cardId) {
+  //   return this._request(`${this._url}/cards/${cardId}/likes`, {
+  //     method: 'PUT',
+  //     headers: {
+  //       authorization: this._authorization,
+  //     }
+  //   })
+  //   .then(this._checkResponse)
+  // }
+
+  deleteCard(cardId) {
+    return this._request(`cards/${cardId}`, {
+      method: 'DELETE'
+  })
+}
+// removeLike(cardId) {
+//   return this._request(`${this._url}/cards/${cardId}/likes`, {
+//     method: 'DELETE',
+//     headers: {
+//       authorization: this._authorization,
+//     }
+//   })
+//   .then(this._checkResponse)
+// }
+
+changeLikeCardStatus(cardId, Isliked) {
+  return this._request(`cards/${cardId}/likes`, {
+    method: Isliked ? 'PUT' : 'DELETE' 
+  })
+}
+}
+
+const api = new Api({
+  baseUrl: 'https://api.denmyname.nomoredomainswork.ru',
+});
+
+export default api
